c4cf9f9ccc1db984a46c1de6cfa69e56bf2f348f
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-10-08 01:15:09 +0400

    Update snap.sh

--- snap.sh
+++ snap.sh
@@ -493,5 +493,4 @@ sudo cp $FILE_PATH_JSON $PUBLIC_FILE_JSON
 # Если хотите увидеть содержимое файла, раскомментируйте следующую строку
 # cat $PUBLIC_FILE_JSON
 systemctl restart ${PR_USER}-snap
-echo наконец перезагружаем!!! test
 done
