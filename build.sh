7c8d64f03bbb2dc34a2c65ccc77c8f7c5bd8a326
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-10-07 22:39:47 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -23,8 +23,8 @@ SLEEP=$(sed -n "/sleep:/s/.*'\([^']*\)'.*/\1/p" snap.conf)
 
 # Выполнить git pull
   cd /home/${PR_USER}/snap
-  sudo -u $PR_USER chown -R ${PR_USER}:${PR_USER} /home/${PR_USER}/snap
-  sudo -u $PR_USER chmod -R 755 /home/${PR_USER}/snap
+  # chown -R ${PR_USER}:${PR_USER} /home/${PR_USER}/snap
+  # chmod -R 755 /home/${PR_USER}/snap
   echo Checking updates...
   git stash
   GIT_PULL_RESULT=$(git pull https://github.com/itrocket-am/snap.git main)
