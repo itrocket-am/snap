5ef8df0263b290494f94c992a659e86697a17fb7
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-10-07 18:56:07 +0400

    Update snap.sh

--- snap.sh
+++ snap.sh
@@ -104,7 +104,7 @@ sleep 3
     MESSAGE="itrocket.net builded and moved to public folder!"
   curl --header 'Content-Type: application/json' --request 'POST' --data '{"chat_id":"'"${CHAT_ID_ALARM}"'", "text":"'"$(echo -e "${MESSAGE}")"'", "parse_mode": "html"}' "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" /dev/null 2>&1
   fi
-  sleep 60
+  sleep 3600
 
 # start script
 for (( ;; )); do
